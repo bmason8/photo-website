6ee9b86644c341375e0e7b3a51816c8ec8805aa2
Ben Mason <ben.mason8@gmail.com>
2017-04-19 22:01:25 -0700

    Added navbar color change when scroll reaches 'Albums' section. Restyled background and header for 'Albums' page. Adjusted grid layout of homepage for better layouts on all screen sizes.

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,19 @@ setTimeout(function() {
     $('.welcome-text').fadeOut('slow');
     $('.sub-welcome-text').fadeOut('slow');
 }, 2000);
+
+var offset = $('#nav-color-change').offset().top;
+var navbar = $('nav');
+var navTextColor = $('.nav-items-color')
+
+$(document).scroll(function() {
+    position = $(this).scrollTop();
+    if (position < offset) {
+        navbar.css('background-color', 'rgba(0, 0, 0, 0)');
+        navTextColor.css('color', 'white');
+    } 
+    else {
+        navbar.css('background-color', '#fff');
+        navTextColor.css('color', 'black');
+    }
+})
